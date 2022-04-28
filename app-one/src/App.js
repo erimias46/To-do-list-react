4f3548429a5b11f71c87e3ffd32e610f6a956f6e
erimias46 <erimias46@gmail.com>
2022-04-28 20:44:00 +0300

    first commit

---
+++ app-one/src/App.js
@@ -0,0 +1,25 @@
+import React,{useState} from "react";
+import './App.css';
+import Form from "./Components/Form";
+import TODO from "./Components/todo";
+
+
+
+
+
+
+function App() {
+  const[inputText,setInputText]=
+  
+  return (
+    
+   <div className="App">
+    
+    <header> To do list</header>
+   <Form/>
+  <TODO/>
+   </div>
+  );
+}
+
+export default App;
---
+++ app-one/src/Components/todo.js
@@ -0,0 +1,10 @@
+import react from "react";
+
+const TODO=()=>{
+    return(<div className="todo-container">
+    <ul className="todo-list"></ul>
+  </div>
+)
+}
+
+export default TODO;
